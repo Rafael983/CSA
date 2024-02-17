9c708886f3391b0677acdd4e969c9cf7bc9a3c34
CSA10158 <schultzezra8@gmail.com>
2024-02-17 12:10:59 -0600

    2-17-24

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
-// console.log(document.getElementById("test").textContent)
-// var x = document.getElementById("test")
-let text_options = ['Great','amazing','insane'];
-function printHere(){
-    document.getElementById("test").textContent="yay";
-    let text_hello = document.getElementById("test").textContent;
-    if(text_hello =="yay"){
-        document.getElementById("test").textContent="bye";
-    }
-    else{
-        document.getElementById("test").textContent="yay"  ;
-    }
-    }
+const heading1 = document.getElementById("text")
+let name= 'Alexandra is sailing the world';
+heading1.textContent = `Hi ${name}`;
+Get_firstcharacter(name);
+Get_lastsebas(name);
+Get_NumberOfChars(name);
+Get_Upper(name);
+Get_Lower(name);
+Get_Capitalize(name);
+function Get_firstcharacter(text){
 
-    const colors = ['red','blue','yellow'];
-    let index=0;
-function switchColor(){
-        document.getElementById('mainButton').style.backgroundColor=colors[index];
-        index++;
-        if(index===3){
-            index=0;
-        }
-        
-    }
+
+    console.log(text.substring(0,1));
+}
+
+function Get_lastsebas(text){
+
+    console.log(text.at(-1));
+}
+
+function Get_NumberOfChars(text){
+
+    console.log(text.length);
+}
+
+function Get_Upper(text){
+    
+    console.log(text.toUpperCase())
+}
+
+function Get_Lower(text){
+
+    console.log(text.toLowerCase())
+}
+
+function Get_Capitalize(text){
+
+    console.log(text.at(0).toUpperCase());
+}
+
+const input_ = document.getElementById("i1")
+
+
+const e1 = document.getElementById('Q1')
+
+
+input_.addEventListener("keyup" , ()=>{
+    let value = input_.value;
+
+    e1.textContent = getlower(value_);
+})
+
+
+
+
